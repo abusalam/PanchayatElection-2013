ca87c9f4bea2e259d3991bfb6cbe2e52e4d4db3c
abusalam <abusalamparvezalam@gmail.com>
2013-07-25 12:00:51 +0530

    Counting Appointment Letters Module Complete

--- CountingAppLetter.php
+++ CountingAppLetter.php
@@ -3,20 +3,18 @@
 include_once 'MyPDF.php';
 include_once 'functions.php';
 session_start();
-$_SESSION['BlockCode'] = 'M01';
-$h = 84;
-$w = 190;
-$gap = 4;
+//$_SESSION['BlockCode'] = 'M01';
+
 $pdf = new PDF();
 $Data = new PanchayatElection\DB();
 switch (PanchayatElection\GetVal($_POST, 'CmdSubmit')) {
-  case "Office-wise Corrigendum Letters":
+  case "Office-wise Appointment Letters":
     $Query = "Select * from " . MySQL_Pre . "CP_Appointments "
             . " Where OfficeCode='" . PanchayatElection\GetVal($_POST, 'off_code')
             . "' AND BlockCode='" . PanchayatElection\GetVal($_SESSION, 'BlockCode') . "'";
     $FileName = 'OffAppLetterCP-[' . PanchayatElection\GetVal($_POST, 'off_code') . '].pdf';
     break;
-  case "Single Corrigendum Letter":
+  case "Single Appointment Letter":
     $Query = "Select * from " . MySQL_Pre . "CP_Appointments "
             . " Where PerCode='" . PanchayatElection\GetVal($_POST, 'PerCode')
             . "' AND BlockCode='" . PanchayatElection\GetVal($_SESSION, 'BlockCode') . "'";
@@ -31,7 +29,10 @@ switch (PanchayatElection\GetVal($_POST, 'CmdSubmit')) {
 $Data->do_sel_query($Query);
 
 $pdf->SetTitle("");
-$pdf->SetMargins(10, 4);
+$pdf->SetMargins(15, 5);
+$h = 120;
+$w = 180;
+$gap = 15;
 $Office = "";
 while ($Row = $Data->get_row()) {
 
@@ -62,32 +63,44 @@ while ($Row = $Data->get_row()) {
   $pdf->Cell(0, 4, '(Under Rule 97)', 0, 1, "C");
   $pdf->SetFont('Arial', 'BU', 11);
   $pdf->Cell(0, 5, 'Appointment of Counting Officers / Assistants', 0, 1, "C");
-  $pdf->Cell(0, 5, 'Election to ' . $Row['BlockName'] . ' Gram Panchayat / Panchayat Samity / Zilla Parishad', 0, 1, "C");
+  $pdf->Cell(0, 5, 'Election to ' . $Row['AssemblyName'] . ' Gram Panchayat / Panchayat Samity / Zilla Parishad', 0, 1, "C");
   $pdf->Ln(2);
-
+  $pdf->SetFont('Arial', 'B', 10);
+  $pdf->Cell(70, 6, 'Name and Number of Constituency(s) ', 0, 0, "L");
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Cell(60, 6, $Row['AssemblyName'], 0, 1, "L");
   $pdf->Ln(2);
-  $pdf->SetFont('Arial', '', 8);
-  $pdf->Write(4, '       I, the Returning Officer of the above mentioned Constituency(s) do hereby appoint the persons'
+  $pdf->Write(5, '       I, the Returning Officer of the above mentioned Constituency(s) do hereby appoint the persons'
           . ' whose names are specified below to act as ' . $Post . ' and to attend the counting venue'
           . ' as mentioned below at 06:30 hours on 29/07/2013 for the purpose of assisting me in the counting of votes'
           . ' at the said election(s).');
-  $pdf->Ln(5);
-  $pdf->SetFont('Arial', 'B', 8);
-  $pdf->Cell(0, 6, $OfficerName . ' [' . $Row['PerCode'] . ']' . ', ' . $Post, 0, 1, "C");
-  $pdf->Ln(2);
-  $pdf->Cell(40, 6, 'Counting Venue: ' . $Row['VenueDCRC'] . ', ' . $Row['AddressDCRC'], 0, 1, "L");
-  $pdf->SetFont('Arial', '', 8);
-  $pdf->Cell(40, 4, 'Training Venue: ' . $Row['TrVenue'], 0, 1, "L");
-  $pdf->Cell(40, 4, 'Training Date: ' . $Row['TrDate'] . ' Time:' . $Row['TrTime'], 0, 1, "L");
-  $pdf->Ln(2);
-  $pdf->SetFont('Arial', '', 8);
-  $pdf->Write(3, 'N.B.: Counting Personnel are instructed not to bring Mobile phones or other electronic gadgets to the'
+  $pdf->Ln(7);
+  $pdf->SetFont('Arial', 'B', 10);
+  $pdf->Cell(0, 6, $Post . ': ' . $OfficerName . ' [' . $Row['PerCode'] . '], ' . $Row['PersDesg'], 0, 1, "L");
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Cell(0, 6, '                                   ' . $Row['OfficeName'], 0, 1, "L");
+  $pdf->SetFont('Arial', 'BU', 10);
+  $pdf->Cell(0, 8, 'Counting Venue: ', 0, 1, "L");
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Write(4, $Row['VenueDCRC'] . ', ' . $Row['AddressDCRC']);
+
+  $pdf->Ln(6);
+  $pdf->SetFont('Arial', 'B', 10);
+  $pdf->Cell(30, 6, 'Training Venue: ', 0, 0, "L");
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Cell(0, 6, $Row['TrVenue'], 0, 1, "L");
+  $pdf->SetFont('Arial', 'B', 10);
+  $pdf->Cell(30, 6, 'Training Date: ', 0, 0, "L");
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Cell(0, 6, $Row['TrDate'] . ' Time: ' . $Row['TrTime'], 0, 1, "L");
+  $pdf->Ln(3);
+  $pdf->SetFont('Arial', '', 10);
+  $pdf->Write(4, 'N.B.: Counting Personnel are instructed not to bring Mobile phones or other electronic gadgets to the'
           . ' Counting Venue. Counting Personnel are instructed to carry their EPIC cards for identification and to '
           . ' bring one Passport size photo for pasting on Identity Card.');
 
-  $pdf->SetY($y + $h - 15);
+  $pdf->SetY($y + $h - 10);
   $pdf->SetFont('Arial', 'B', 8);
-  $pdf->Cell(30, 6, 'Office: ' . $Row['OfficeName'], 0, 1, "L");
   $pdf->Cell(30, 4, 'Place: Paschim Medinipur', 0, 1, "L");
   $pdf->Cell(30, 4, 'Date: ' . date('d/m/Y'), 0, 1, "L");
 
@@ -111,8 +124,8 @@ while ($Row = $Data->get_row()) {
     $pdf->AddPage();
     $y%=$h * $rpp; */
 }
-$pdf->Output();
-//$pdf->Output($FileName, 'D');
+//$pdf->Output();
+$pdf->Output($FileName, 'D');
 unset($pdf);
 exit();
 ?>
--- ReportCounting.php
+++ ReportCounting.php
@@ -4,14 +4,11 @@ use PanchayatElection as PE;
 
 require_once('functions.php');
 
-if (PE\GetVal($_POST, 'CmdSubmit') === "Download Notice") {
-  include 'ElectionNotice.php';
-}
 switch (PE\GetVal($_POST, 'CmdSubmit')) {
-  case "Download All Corrigendum Letters":
-  case "Office-wise Corrigendum Letters":
-  case "Single Corrigendum Letter":
-    include 'AppointmentLetter.php';
+  case "Download All Appointment Letters":
+  case "Office-wise Appointment Letters":
+  case "Single Appointment Letter":
+    include 'CountingAppLetter.php';
     break;
 }
 PE\srer_auth();
@@ -22,24 +19,6 @@ PE\HtmlHeader("Report");
   @import url("css/Style.css");
   -->
 </style>
-<link type="text/css" href="css/ui-darkness/jquery-ui-1.8.21.custom.css"
-      rel="Stylesheet" />
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
-<script type="text/javascript">
-  $(function() {
-    $(".datepick").datepicker({minDate: "-43Y",
-      maxDate: "-18Y",
-      dateFormat: 'yy-mm-dd',
-      showOtherMonths: true,
-      selectOtherMonths: true,
-      showButtonPanel: true,
-      changeMonth: true,
-      changeYear: true,
-      showAnim: "slideDown"
-    });
-  });
-</script>
 </head>
 <body>
   <div class="TopPanel">
@@ -75,30 +54,16 @@ PE\HtmlHeader("Report");
           ?>
         </select>
         <input type="submit" name="CmdSubmit" value="Show Data"/>
-       <!-- <input type="submit" name="CmdSubmit" value="Download Notice"/>
-
-        <input type="submit" name="CmdSubmit" value="Office-wise Corrigendum Letters"/>
-
-        <label for="ChkShow"><input type="checkbox" name="ChkShow" id="ChkShow" value="NOT" checked="checked"/>Hide Deleted Data</label> -->
+        <input type="submit" name="CmdSubmit" value="Office-wise Appointment Letters"/>
+        <label for="ChkShow">Hide Deleted Data</label>
+        <input type="checkbox" name="ChkShow" id="ChkShow" value="NOT" checked="checked"/>
       </div>
       <div style="clear:both;"></div>
       <hr />
-      <!--input type="submit" name="CmdQuery" value="Ofice wise Others Count"/>
-      <input type="submit" name="CmdQuery" value="Scalewise Personnel Count"/>
-      <input type="submit" name="CmdQuery" value="Error B.Pay"/>
-      <input type="submit" name="CmdQuery" value="Unmatched Scale"/>
-      <input type="submit" name="CmdQuery" value="No Staff"/>
-      <input type="submit" name="CmdQuery" value="All P4"/>
-      <input type="submit" name="CmdQuery" value="Invalid DOB"/>
-      <input type="submit" name="CmdQuery" value="Invalid B.Pay"/>
-      <input type="submit" name="CmdQuery" value="Invalid Scale"/-->
-      <!--input type="submit" name="CmdSubmit" value="Download All Corrigendum Letters"/>
-      <input type="text" name="PerCode" value="Personnel ID"/>
-      <input type="submit" name="CmdSubmit" value="Single Corrigendum Letter"/>
-      <input type="submit" name="CmdSubmit" value="Party vs PS"/>
-      <input type="submit" name="CmdSubmit" value="PS vs Party"/-->
+      <input type="submit" name="CmdSubmit" value="Download All Appointment Letters"/>
+      <input type="text" name="PerCode" placeholder="Personnel ID"/>
+      <input type="submit" name="CmdSubmit" value="Single Appointment Letter"/>
       <hr />
-      <div style="clear:both;"></div>
     </form>
     <div style="clear:both;"></div>
     <br/>
@@ -112,27 +77,7 @@ PE\HtmlHeader("Report");
                 . "  Where NOT Deleted AND O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' Group By O.off_code";
         PE\ShowData($Qry);
         break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Scalewise Personnel Count"):
-        $Qry = "Select description, count(per_code) as `Actual Count` "
-                . " from " . MySQL_Pre . "office O INNER JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P "
-                . "ON (O.off_code=P.off_code) INNER JOIN " . MySQL_Pre . "scale S ON S.scalecode=P.scalecode Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' Group By P.scalecode";
-        PE\ShowData($Qry);
-        //$_SESSION['Msg']=$Qry;
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Error B.Pay"):
-        $Qry = "Select P.per_code,P.officer_nm,DATE_FORMAT(date_ob,'%d/%m/%Y') as date_ob,pay "
-                . " from " . MySQL_Pre . "office O INNER JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted AND pay<6600) P "
-                . "ON (O.off_code=P.off_code) Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "'";
-        PE\ShowData($Qry);
-        //$_SESSION['Msg']=$Qry;
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Unmatched Scale"):
-        $Qry = "Select O.off_code,P.per_code,P.officer_nm,DATE_FORMAT(date_ob,'%d/%m/%Y') as date_ob,pay,S.description "
-                . " from " . MySQL_Pre . "office O INNER JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P "
-                . "ON (O.off_code=P.off_code) INNER JOIN " . MySQL_Pre . "scale S ON (S.scalecode=P.scalecode) Where P.scalecode<14 AND O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "'";
-        PE\ShowData($Qry);
-        //$_SESSION['Msg']=$Qry;
-        break;
+
       case (PE\GetVal($_POST, 'CmdSubmit') === "Show Data"):
         $Qry = "Select P.PersSL,P.officer_nm,P.OFF_DESC,M.gender_desc,P.BASICpay,N.status_desc,"
                 . " P.mobile,X.block_muni_nm "
@@ -143,52 +88,6 @@ PE\HtmlHeader("Report");
         echo "<B>Office: </b>"
         . $Data->do_max_query("Select CONCAT('[',off_code,'] - [',office,'] - [',address1,']') "
                 . "from " . MySQL_Pre . "office Where off_code='" . PE\GetVal($_POST, 'off_code') . "'") . "<br/>";
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "No Staff"):
-        $Qry = "Select O.off_code,office,address1,count(per_code) as `Staff Count` "
-                . " from " . MySQL_Pre . "office O LEFT JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P ON (O.off_code=P.off_code) "
-                . " Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' Group By O.off_code having count(per_code)<1";
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "All P4"):
-        $Qry = "Select O.off_code,office,totstaff,count(PostStatus) as `P4 Count` "
-                . " from " . MySQL_Pre . "office O LEFT JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P ON (O.off_code=P.off_code) "
-                . "left join " . MySQL_Pre . "scale S on (S.scalecode=P.scalecode and S.PostStatus='P4')"
-                . " Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' Group By O.off_code having (totstaff=count(PostStatus) and count(PostStatus)>0)";
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Invalid DOB"):
-        $Qry = "Select O.off_code,office,per_code,P.officer_nm,date_ob"
-                . " from " . MySQL_Pre . "office O LEFT JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P ON (O.off_code=P.off_code) "
-                . " Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' and (date_ob='1970-01-01' or date_ob<='1953-07-01')";
-        PE\ShowData($Qry);
-        //$_SESSION['Msg']=$Qry;
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Invalid B.Pay"):
-        $Qry = "Select O.off_code,office,per_code,P.officer_nm,pay "
-                . " from " . MySQL_Pre . "office O LEFT JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P ON (O.off_code=P.off_code) "
-                . " Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' and (pay<4000 or pay>40000)";
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdQuery') === "Invalid Scale"):
-        $Qry = "Select O.off_code,office,per_code,P.officer_nm,description,pay"
-                . " from " . MySQL_Pre . "office O LEFT JOIN (Select * from " . MySQL_Pre . "personnel Where NOT Deleted) P ON (O.off_code=P.off_code) "
-                . "left join " . MySQL_Pre . "scale S on (S.scalecode=P.scalecode)"
-                . " Where O.blockmuni='" . PE\GetVal($_SESSION, 'BlockCode') . "' and S.PostStatus='P0'";
-        //$_SESSION['Msg']=$Qry;
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdSubmit') === "PS vs Party"):
-        $Qry = "Select LPAD(groupid,3,'0') as `Party No`,CONCAT('[',pscd,'] - ',psnm) as `Polling Station` from  " . MySQL_Pre . "PS "
-                . " Where assembly='" . PE\GetVal($_SESSION, 'BlockCode') . "' Order By pscd";
-        $_SESSION['Msg'] = "<b>" . PE\GetVal($_SESSION, 'BlockCode') . ":</b> Update Complete";
-        PE\ShowData($Qry);
-        break;
-      case (PE\GetVal($_POST, 'CmdSubmit') === "Party vs PS"):
-        $Qry = "Select LPAD(groupid,3,'0') as `Party No`,CONCAT('[',pscd,'] - ',psnm) as `Polling Station` from  " . MySQL_Pre . "PS "
-                . " Where assembly='" . PE\GetVal($_SESSION, 'BlockCode') . "' Order By groupid";
-        $_SESSION['Msg'] = "<b>" . PE\GetVal($_SESSION, 'BlockCode') . ":</b> Update Complete";
         PE\ShowData($Qry);
         break;
       default:
